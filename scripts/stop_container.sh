194d2e9a8e7ad090493a096d801591f06b5df8a8
Abhi Bhuva <bhuvaabhi72@gmail.com>
2024-02-15 15:58:29 +0530

    dsx

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Get the container ID running on port 3000
-sudo docker rm -f $(sudo docker ps -a -q)
-sudo docker rmi $(docker images -f "dangling=true" -q)
+#sudo docker rm -f $(sudo docker ps -a -q)
+#sudo docker rmi $(docker images -f "dangling=true" -q)
