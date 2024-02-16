8e19e2aed3875c816c9a433e6c0fc7d1ef280156
Abhi Bhuva <bhuvaabhi72@gmail.com>
2024-02-16 14:26:52 +0530

    first commit

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,10 +2,10 @@
 set -e
 
 # Get the container ID running on port 3000
-#sudo docker rm -f $(sudo docker ps -a -q)
-#sudo docker rmi $(docker images -f "dangling=true" -q)
-containerid="$(sudo docker ps -q)"
-sudo docker rm -f "$containerid"
+sudo docker rm -f $(sudo docker ps -a -q)
+sudo docker rmi $(docker images -f "dangling=true" -q)
+#containerid="$(sudo docker ps -q)"
+#sudo docker rm -f "$containerid"
 #containerid='docker ps | awk -F " " '{ print $1}'
 #containerid='docker ps -q '
 #docker rm -f $containerid
