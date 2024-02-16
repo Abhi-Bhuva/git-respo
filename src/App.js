50fcc48a0737b53b2abc1da9b4dae07c366b4d9f
Abhi Bhuva <bhuvaabhi72@gmail.com>
2024-02-16 14:45:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank" 
           rel="noopener noreferrer"
         >
-        React-js update fully  done
+        React-js
         </a>
       </header>
     </div>
